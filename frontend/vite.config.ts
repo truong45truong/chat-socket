14ff191d5050b60920ff73ca8b8addd8e6493ef4
= <truong45bi@gmail.com>
2023-09-06 01:51:36 +0700

    fix deploy nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host : "34.238.160.245",
+    host : "0.0.0.0",
     port: 8080,
   },
   plugins: [
