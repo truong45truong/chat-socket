c89c280d2b9e658a771a9244b55f4f17cf474801
= <truong45bi@gmail.com>
2023-09-06 13:18:08 +0700

    update fix socket

--- backend/chat_base/settings_dev.py
+++ backend/chat_base/settings_dev.py
@@ -108,7 +108,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [( "0.0.0.0", "6379")],
+            "hosts": [( "redis", "6379")],
         },
     },
 }
